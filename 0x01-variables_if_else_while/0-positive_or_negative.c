0ac8edae2d7b300ce29bec94fea03f372e1b2a52
Juliarita <nkechi.iheo@gmail.com>
2022-09-12 13:34:44 +0100

    print a program on if, else if, else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+#include <stdio.h>
+
 int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-/* your code goes there */
-	return (0);
-}
+	if (n > 0)
+	{
+		printf("%i is positive", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative", n);
+	}
+	else
+	{
+		printf("%i is zero", n);
+	}
+	return (0); 
+} 
