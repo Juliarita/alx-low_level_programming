49ef43b0f924d50810141546961213dda82977ae
Juliarita <nkechi.iheo@gmail.com>
2022-09-12 17:30:37 +0100

    Fix line break issues

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
 */
 int main(void)
 {
-	fwrite("and that piece of art is useful
-		\" -Dora Korpar, 2015-10-19", 59);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
