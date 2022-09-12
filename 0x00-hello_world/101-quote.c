19f704a5cfacfec078a8e89feed55aefdb52fbc7
Juliarita <nkechi.iheo@gmail.com>
2022-09-12 17:20:58 +0100

    Completes task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-  #include <stdio.h>
-  #include <stdlib.h>
-  /**
-   * main - Entry point
-   * Return: Alway 1 (Success)
-   */
-  int main(void)
- 
-  {
-	
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" -Dora Korpar, 2015-10-19", 59);
+ #include <stdio.h>
+ #include <stdlib.h>
+
+/**
+* main - Entry point
+* Descritpion: Write to the standard error output
+* Return: Alway 1 (Success)
+*/
+int main(void)
+{
+	fwrite("and that piece of art is useful
+		\" -Dora Korpar, 2015-10-19", 59);
 	return (1);
-  } 
+}
