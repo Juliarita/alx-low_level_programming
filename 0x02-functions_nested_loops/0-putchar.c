f4f1e2dfab030b0fc9289a61e2c25a0127e6623e
Juliarita <nkechi.iheo@gmail.com>
2022-09-14 12:10:03 +0100

    please be solved

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
 /**
- * main - prints Putchar
- *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+* main - I code like a star
+* Description: using the main function
+* this program prints _putchar
+* Return: 0
 */
-
-int _puchar(char c); 
+int main(void)
 {
-	_putchar('0');
+	char var[] = "_putchar";
+	int i;
+	int lenVar = sizeof(var) / sizeof(var[0]);
+	for (i = 0; i < lenVar - 1; i++)
+	{
+		_putchar(var[i]);
+	}
 	_putchar('\n');
-
 	return (0);
 }
