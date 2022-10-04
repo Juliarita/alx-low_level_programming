8738ecf00cfee20aafb01e06d566474eb5a8ba91
Juliarita <nkechi.iheo@gmail.com>
2022-10-04 01:48:23 +0100

    project on malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* create_array - this function is to call an array of chars
+* @size : inegter permater of the arrays 
+* @array : chars of array 
+* return 0
+*/
+
+char *create_array(unsigned int size, char c);
+{
+
+	return 0;
+}
+ 
