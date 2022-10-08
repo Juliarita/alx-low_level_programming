1fdd7d90bcb2ca0ad2b1b29414c0d4fbad2098f1
Juliarita <nkechi.iheo@gmail.com>
2022-10-08 01:35:53 +0100

    more on malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*  _calloc - this function is allocateing a memory for an array using malloc.
+* @nmemb: parameter use for the elements of the array
+* @size: parameter use for the length of the array
+* Return: pointer to the allocated memory
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *n;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	n = calloc(nmemb, size);
+
+	if (n == NULL)
+	return (n);
+}
