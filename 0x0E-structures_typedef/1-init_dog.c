daa9ecaf03988ea0be9d66dd613e7046693cca44
Juliarita <nkechi.iheo@gmail.com>
2022-10-11 01:45:30 +0100

    struture and typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,6 @@
 * @name: parameter name the username
 * @age: parameter age the userage
 * @owner: parameter owner the userowner
-* return (0);
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -17,7 +16,5 @@ if (d != NULL)
 d->name = name;
 d->age = age;
 d->owner = owner;
-
 }
-
 }
