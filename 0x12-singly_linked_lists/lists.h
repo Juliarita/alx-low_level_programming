416c405a907c11e7dca4869a8ebfdd4caa161773
Juliarita <nkechi.iheo@gmail.com>
2022-10-21 07:01:44 +0100

    list free

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifnedef LIST_H
+#ifndef LIST_H
 #define LIST_H
 
 /**
@@ -15,5 +15,17 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
+
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+
+#endif
