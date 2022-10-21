b175c135c28afb5481ccb5a70248caa463c75f25
Juliarita <nkechi.iheo@gmail.com>
2022-10-21 07:16:35 +0100

    list free

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node to the list
+ * add_node_end - Adds a new node to the end of the list
  * @head: The head of the list
  * @str: The string to be added
  * Return: Returns the address of the new node or null
  * if it failed.
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t len;
 	int i;
+	list_t *node;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -23,8 +24,19 @@ list_t *add_node(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = len;
 
-	new->next = *head;
-	(*head) = new;
 
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	node = *head;
+
+	for (i = 0; node->next != NULL; i++, node = node->next)
+	{
+		continue;
+	}
+
+	node->next = new;
 	return (new);
 }
